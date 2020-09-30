60521cffd9202af9aa772d6523d24e904d69b105
Satwik Ram <satwikram29@gmail.com>
2020-09-30 21:11:49 +0530

    Updated

--- post/views.py
+++ post/views.py
@@ -25,28 +25,27 @@ def article_list(request):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def article_detail(request, pk):
     try:
         article = Article.objects.get(pk = pk)
 
     except Article.DoesNotExist:
-        return HttpResponse(status = 404)
+        return HttpResponse(status = status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = ArticleSerializers(article)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializers(article, data = data)
+        serializer = ArticleSerializers(article, data = request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status = 400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         article.delete()
-        return HttpResponse(status = 204)
+        return Response(status = status.HTTP_204_NO_CONTENT)
 
