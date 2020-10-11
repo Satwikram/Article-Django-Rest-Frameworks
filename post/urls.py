072b288300c5d0f18bc6fe6dbdcbe7fdffd1cb98
Satwik Ram <satwikram29@gmail.com>
2020-10-11 11:45:40 +0530

    Urls added

--- post/urls.py
+++ post/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
         #path('article/', article_list),
         path('article/', ArticleAPIView.as_view()),
-        path('detail/<int:id>/', ArticleDetailsAPIView.as_view())
+        path('detail/<int:id>/', ArticleDetailsAPIView.as_view(),
         #path('detail/<int:id>/', article_detail),
-
+        path('comment/', CommentAPIView.as_view()),
 
 ]
